f30ea17adf51d14f5102a4d9cd140846df51d233
Hromas <90126334+HromasDev@users.noreply.github.com>
2023-05-21 23:28:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,169 @@
-let body = document.body;
-let world = document.getElementById("world")
-let input = document.getElementById("keyinput")
-let inventory = document.getElementById("inventory")
-let layers = document.getElementsByClassName("layer")
-let inventorygrid = document.getElementById("inventorygrid")
-let player = document.getElementsByClassName("player")[0]
-inventory.style.visibility = "hidden"
+let map = ['', '', '', '', '', '', '', '', '']
+let roles = ['X', 'O']
 
-
-let chunks = 2;
-let chunkblocks = 10;
-
-for (let i = 0; i < 8; i++) {
-    world.innerHTML += '<div class="layer"></div>'
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
-for (let i = 0; i < 2; i++) {
-    for (let j = 0; j < 20; j++) {
-        layers[i].innerHTML += '<div class="block grass"></div>'
+let currentrole = roles[getRandomInt(2)]
+
+let botrole;
+
+if (currentrole == 'O') botrole = 'X';
+else botrole = 'O'
+
+
+console.log('Ваша роль ' + currentrole)
+console.log('Роль бота ' + botrole)
+
+let buttons = document.getElementsByClassName('button');
+
+function result() {
+    if (buttons[0].value == currentrole && buttons[1].value == currentrole && buttons[2].value == currentrole || buttons[3].value == currentrole && buttons[4].value == currentrole && buttons[5].value == currentrole || buttons[6].value == currentrole && buttons[7].value == currentrole && buttons[8].value == currentrole || buttons[0].value == currentrole && buttons[3].value == currentrole && buttons[6].value == currentrole || buttons[1].value == currentrole && buttons[4].value == currentrole && buttons[7].value == currentrole || buttons[2].value == currentrole && buttons[5].value == currentrole && buttons[8].value == currentrole || buttons[0].value == currentrole && buttons[4].value == currentrole && buttons[8].value == currentrole || buttons[2].value == currentrole && buttons[4].value == currentrole && buttons[6].value == currentrole) {
+        alert('Ты победил')
+        return
     }
-    for (let j = 0; j < 20; j++) {
-        
+
+    else if (buttons[0].value == botrole && buttons[1].value == botrole && buttons[2].value == botrole || buttons[3].value == botrole && buttons[4].value == botrole && buttons[5].value == botrole || buttons[6].value == botrole && buttons[7].value == botrole && buttons[8].value == botrole || buttons[0].value == botrole && buttons[3].value == botrole && buttons[6].value == botrole || buttons[1].value == botrole && buttons[4].value == botrole && buttons[7].value == botrole || buttons[2].value == botrole && buttons[5].value == botrole && buttons[8].value == botrole || buttons[0].value == botrole && buttons[4].value == botrole && buttons[8].value == botrole || buttons[2].value == botrole && buttons[4].value == botrole && buttons[6].value == botrole) {
+        alert('Ты проиграл')
+        return
     }
 }
 
-for (let i = 0; i < 20; i++) {
-    inventorygrid.innerHTML += '<div class="item"></div>';
+function push1() {
+    let botstep = null;
+
+    while (botstep == null || botstep == 0 || map[botstep] !== '') botstep = getRandomInt(9);
+
+    if (map[0] == 0) {
+        document.getElementsByClassName('button')[0].value = currentrole;
+        document.getElementsByClassName('button')[botstep].value = botrole;
+
+        map[0] = currentrole
+        map[botstep] = botrole
+    }
+    console.log(map)
+    result()
 }
 
-let x = 0;
-let y = 0;
-let z = 0;
+function push2() {
+    let botstep = null;
 
-let stepSize = 20
-let inJump = false;
+    while (botstep == null || botstep == 1 || map[botstep] !== '') botstep = getRandomInt(9);
 
-input.addEventListener("keydown", function (board) {
-    console.log(board.keyCode);
-    if (board.keyCode === 73) {
-        if (inventory.style.visibility == "hidden") inventory.style.visibility = "visible"
-        else inventory.style.visibility = "hidden";
+    if (map[1] == 0) {
+        document.getElementsByClassName('button')[1].value = currentrole;
+        document.getElementsByClassName('button')[botstep].value = botrole;
+
+        map[1] = currentrole
+        map[botstep] = botrole
+    }
+    console.log(map)
+    result()
+}
+
+function push3() {
+    let botstep = null;
+
+    while (botstep == null || botstep == 2 || map[botstep] !== '') botstep = getRandomInt(9);
+    if (map[2] == 0) {
+        document.getElementsByClassName('button')[2].value = currentrole;
+        document.getElementsByClassName('button')[botstep].value = botrole;
+
+        map[2] = currentrole
+        map[botstep] = botrole
     }
 
-    else if (board.keyCode === 68) {
-        player.style.left = x + "px";
-        x = x + stepSize;
+    console.log(map)
+    result()
+}
+
+function push4() {
+    let botstep = null;
+
+    while (botstep == null || botstep == 3 || map[botstep] !== '') botstep = getRandomInt(9);
+    if (map[3] == 0) {
+        document.getElementsByClassName('button')[3].value = currentrole;
+        document.getElementsByClassName('button')[botstep].value = botrole;
+
+        map[3] = currentrole
+        map[botstep] = botrole
     }
+    console.log(map)
+    result()
+}
 
-    else if (board.keyCode === 65) {
-        player.style.left = x + "px";
-        x = x - stepSize;
+function push5() {
+    let botstep = null;
+
+    while (botstep == null || botstep == 4 || map[botstep] !== '') botstep = getRandomInt(9);
+    if (map[4] == 0) {
+        document.getElementsByClassName('button')[4].value = currentrole;
+        document.getElementsByClassName('button')[botstep].value = botrole;
+
+        map[4] = currentrole
+        map[botstep] = botrole
     }
+    console.log(map)
+    result()
+}
 
-    else if (board.keyCode === 87 && inJump == false) {
-        inJump = true;
-        player.style.animation = 'jump 0.5s linear';
-        setTimeout(() => {
-            player.style.animation = 'none';
-            inJump = false;
-        }, 500)
+function push6() {
+    let botstep = null;
 
+    while (botstep == null || botstep == 5 || map[botstep] !== '') botstep = getRandomInt(9);
+    if (map[5] == 0) {
+        document.getElementsByClassName('button')[5].value = currentrole;
+        document.getElementsByClassName('button')[botstep].value = botrole;
 
-
+        map[5] = currentrole
+        map[botstep] = botrole
     }
-})
+    console.log(map)
+    result()
+}
 
-input.addEventListener("blur", function () {
-    input.focus()
-})
+function push7() {
+    let botstep = null;
+
+    while (botstep == null || botstep == 6 || map[botstep] !== '') botstep = getRandomInt(9);
+    if (map[6] == 0) {
+        document.getElementsByClassName('button')[6].value = currentrole;
+        document.getElementsByClassName('button')[botstep].value = botrole;
+
+        map[6] = currentrole
+        map[botstep] = botrole
+    }
+    console.log(map)
+    result()
+}
+
+function push8() {
+    let botstep = null;
+
+    while (botstep == null || botstep == 7 || map[botstep] !== '') botstep = getRandomInt(9);
+    if (map[7] == 0) {
+        document.getElementsByClassName('button')[7].value = currentrole;
+        document.getElementsByClassName('button')[botstep].value = botrole;
+
+        map[7] = currentrole
+        map[botstep] = botrole
+    }
+    console.log(map)
+    result()
+}
+
+function push9() {
+    let botstep = null;
+
+    while (botstep == null || botstep == 8 || map[botstep] !== '') botstep = getRandomInt(9);
+    if (map[8] == 0) {
+        document.getElementsByClassName('button')[8].value = currentrole;
+        document.getElementsByClassName('button')[botstep].value = botrole;
+
+        map[8] = currentrole
+        map[botstep] = botrole
+    }
+    console.log(map)
+    result()
+}
